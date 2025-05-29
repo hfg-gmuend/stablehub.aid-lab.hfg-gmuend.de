82ee8959d62ac3bc53ada85b0455d1c110b908c6
Christopher Pietsch <cpietsch@gmail.com>
2025-05-29 08:21:33 +0000

    fix: Comment out base path configuration in SvelteKit config

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/stablehub.aid-lab.hfg-gmuend.de' : ''
+			//base: process.env.NODE_ENV === 'production' ? '/stablehub.aid-lab.hfg-gmuend.de' : ''
 		}
 	}
 };
